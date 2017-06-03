0b17f1246a31cb3ad0b3858bbaf0a25af695dcbf
KuKKoMaH <kukkoman@gmail.com>
2017-06-03 11:39:20 +0300

    resolve #5

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,16 +1,1 @@
-webpackJsonp([0],[
-/* 0 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__(0);
-
-
-/***/ })
-],[1]);
+webpackJsonp([0],[function(n,t,c){"use strict"},function(n,t,c){n.exports=c(0)}],[1]);
